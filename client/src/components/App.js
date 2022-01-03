70d3fa401e55f4bbfa5776793d559c3cd014f42a
Jordan Yee <jordanyee799@gmail.com>
2022-01-03 11:39:36 -0800

    include plain bootstrap

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {Route, Switch} from "react-router-dom"
 // import '../App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from "./Login.js"
 import Signup from "./Signup.js"
 import Albums from "./Albums.js";
