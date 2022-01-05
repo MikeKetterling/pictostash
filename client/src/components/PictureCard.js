d53f9ae1280d7b97d0a0041fa107e27801849720
Jordan Yee <jordanyee799@gmail.com>
2022-01-04 19:14:02 -0800

    add carousel

--- client/src/components/PictureCard.js
+++ client/src/components/PictureCard.js
@@ -1,18 +1,18 @@
 import {Card, Button, Col} from 'react-bootstrap'
 
-function PictureCard() {
+function PictureCard({imgUrl, handleShowImg}) {
     return (
         <div className="picture-card">
             <Col className='my-5 mx-3'>
                 <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="https://picsum.photos/100/100" />
+                    <Card.Img className='card-img' variant="top" src={imgUrl} />
                     <Card.Body>
                         <Card.Title>Card Title</Card.Title>
                         <Card.Text>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
                         </Card.Text>
-                        <Button variant="primary">View Image</Button>
+                        <Button variant="primary" onClick={handleShowImg}>View Image</Button>
                     </Card.Body>
                 </Card>
             </Col>
