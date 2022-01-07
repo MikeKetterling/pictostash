761cc2cd0ee5dcfe844705d373426f9f5fe2310d
Jordan Yee <jordanyee799@gmail.com>
2022-01-07 08:52:23 -0800

    remove picture card info

--- client/src/components/PictureCard.js
+++ client/src/components/PictureCard.js
@@ -6,12 +6,7 @@ function PictureCard({imgUrl, handleShowImg, currId}) {
             <Col className='my-5 mx-3'>
                 <Card style={{ width: '18rem' }}>
                     <Card.Img className='card-img' variant="top" src={imgUrl} style={{'objectFit': 'cover'}} />
-                    <Card.Body style={{"backgroundColor": "#deb185"}}>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                        </Card.Text>
+                    <Card.Body style={{"backgroundColor": "#deb185", "textAlign": "center"}}>
                         <Button variant="primary" onClick={() => handleShowImg(currId)}>View Image</Button>
                     </Card.Body>
                 </Card>
